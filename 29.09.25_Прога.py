baebcaf1861c748af463c5482e780e64968665e3
Vozderjus <107810054+Memkased@users.noreply.github.com>
2025-09-29 17:43:49 +0500

    Update 29.09.25_Прога.py

--- 29.09.25_Прога.py
+++ 29.09.25_Прога.py
@@ -1,9 +1,9 @@
-# Конвертируем темпу
+#1
 # n = int(input())
 # print(str(n) + 'C = ' + str((n*9.5) + 32) + 'F') 
 # print(str(n) + 'C = ' + str(n + 273.15) + 'K')
 
-# Проверка числа
+#2
 # n = int(input())
 
 # if n % 2 == 0:
@@ -23,11 +23,20 @@
 # else:
 #     print('Не принадлежит диапазону [10;50]')
 
+#3
 # import random
 # from string import * 
-# letters = ''.join(random.choices(ascii_uppercase, k=3))
-# numbers = ''.join(random.choices('0123456789',  k=3))
-# symbols = ''.join(random.choices('!@#$%^&*', k=2))
-# p = list(letters+numbers+symbols)
-# random.shuffle(p)
+# letters = (random.choices(ascii_uppercase, k=3))
+# numbers = random.choices('0123456789',  k=3)
+# symbols = random.choices('!@#$%^&*', k=2)
+# random.shuffle(letters+numbers+symbols)
 # print(''.join(p))
+
+#4
+# s = input().lower()
+
+# counts = dict()
+# for i in s:
+#     counts[i] = counts.get(i, 0) + 1
+
+# print('\n'.join(f'{i[0]}: {i[1]}' for i in sorted(counts.items(), key=lambda x: x[1], reverse=True)[:3]))
